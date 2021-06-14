31a661a56c22ba05f0df4c871fcad565bfda8fed
Anushreya Satish <75848533+Anushreya-Satish@users.noreply.github.com>
2021-06-14 23:32:18 +0530

    Add files via upload

---
+++ code.js
@@ -0,0 +1,21 @@
+
+playSound("assets/birthday-music.mp3", true);
+
+onEvent("button1", "click", function( ) {
+	setScreen("screen2");
+});
+onEvent("button2", "click", function( ) {
+	setScreen("screen3");
+});
+onEvent("button3", "click", function( ) {
+	setScreen("screen4");
+});
+onEvent("button4", "click", function( ) {
+	setScreen("screen5");
+});
+onEvent("button5", "click", function( ) {
+	setScreen("screen6");
+});
+onEvent("button8", "click", function( ) {
+	setScreen("screen7");
+});
